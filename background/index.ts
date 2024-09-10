376bf30a2087539a268cddd8e96a9bb216400605
ayoung19 <andyluyoung@gmail.com>
2024-09-09 19:12:47 -0700

    fix: cleaner offscreen document creation

--- background/index.ts
+++ background/index.ts
@@ -2,33 +2,19 @@ import OFFSCREEN_DOCUMENT_PATH from "url:~offscreen.html";
 
 import { setClipboardMonitorIsEnabled } from "~storage/clipboardMonitorIsEnabled";
 
-let creating: Promise<void> | undefined; // A global promise to avoid concurrency issues
-async function setupOffscreenDocument() {
-  // Check all windows controlled by the service worker to see if one
-  // of them is the offscreen document with the given path
-  const existingContexts = await chrome.runtime.getContexts({
-    documentUrls: [OFFSCREEN_DOCUMENT_PATH],
-  });
-
-  if (existingContexts.length > 0) {
+const setupOffscreenDocument = async () => {
+  if (await chrome.offscreen.hasDocument()) {
     return;
   }
 
-  // create offscreen document
-  if (creating) {
-    await creating;
-  } else {
-    creating = chrome.offscreen.createDocument({
-      url: OFFSCREEN_DOCUMENT_PATH,
-      reasons: [chrome.offscreen.Reason.CLIPBOARD],
-      justification: "Write text to the clipboard.",
-    });
-    await creating;
-    creating = undefined;
-  }
-}
+  await chrome.offscreen.createDocument({
+    url: OFFSCREEN_DOCUMENT_PATH,
+    reasons: [chrome.offscreen.Reason.CLIPBOARD],
+    justification: "Write text to the clipboard.",
+  });
+};
 
-setupOffscreenDocument();
+chrome.tabs.onActivated.addListener(() => setupOffscreenDocument());
 
 chrome.runtime.onInstalled.addListener(async () => {
   await Promise.all([setClipboardMonitorIsEnabled(true), setupOffscreenDocument()]);
