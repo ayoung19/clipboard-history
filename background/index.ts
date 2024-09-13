64e0f16046e788880d2bcf2318f98746c1a23380
ayoung19 <andyluyoung@gmail.com>
2024-09-12 19:34:10 -0700

    Revert "feat: migrate entries from old namespace to new namespace"
    This reverts commit 388c86534940b8ca7a729b0a4fbfe027dee8027e.

--- background/index.ts
+++ background/index.ts
@@ -1,35 +1,8 @@
 import OFFSCREEN_DOCUMENT_PATH from "url:~offscreen.html";
 
-import { Storage } from "@plasmohq/storage";
-
 import { setClipboardMonitorIsEnabled } from "~storage/clipboardMonitorIsEnabled";
-import { Entry } from "~types/entry";
-import { setEntries } from "~utils/storage";
-
-const entryIdSetNamespaceRemoveAll = async () => {
-  const entryIdSetStorage = new Storage({
-    area: "local",
-  });
-  entryIdSetStorage.setNamespace("entryIdSet");
-
-  return await entryIdSetStorage.removeAll();
-};
-
-const storage = new Storage({
-  area: "local",
-});
 
 const setupOffscreenDocument = async () => {
-  // TODO: Remove this migration.
-  // BEGIN: Migrate entries over to new storage namespace.
-  const oldEntries = await storage.get<Entry[]>("entryIdSetentries");
-  if (oldEntries !== undefined && oldEntries.length > 0) {
-    await setEntries(oldEntries);
-    await storage.remove("clipboardSnapshot");
-    await entryIdSetNamespaceRemoveAll();
-  }
-  // END: Migrate entries over to new storage namespace.
-
   if (await chrome.offscreen.hasDocument()) {
     return;
   }
