85fc9aa53b46eac7625f791a73a52b3d55428375
ayoung19 <andyluyoung@gmail.com>
2024-09-16 23:58:57 -0700

    chore: improve offscreen document setup

--- background/index.ts
+++ background/index.ts
@@ -7,16 +7,24 @@ import {
 import { setActionBadgeText, setActionIconAndBadgeBackgroundColor } from "~utils/actionBadge";
 import { getEntries } from "~utils/storage";
 
+// A global promise to avoid concurrency issues.
+let creating: Promise<void> | null = null;
 const setupOffscreenDocument = async () => {
   if (await chrome.offscreen.hasDocument()) {
     return;
   }
 
-  await chrome.offscreen.createDocument({
-    url: OFFSCREEN_DOCUMENT_PATH,
-    reasons: [chrome.offscreen.Reason.CLIPBOARD],
-    justification: "Write text to the clipboard.",
-  });
+  if (creating) {
+    await creating;
+  } else {
+    creating = chrome.offscreen.createDocument({
+      url: OFFSCREEN_DOCUMENT_PATH,
+      reasons: [chrome.offscreen.Reason.CLIPBOARD],
+      justification: "Read text from clipboard.",
+    });
+    await creating;
+    creating = null;
+  }
 };
 
 const setupAction = async () => {
@@ -33,6 +41,10 @@ const setupAction = async () => {
 
 chrome.tabs.onActivated.addListener(async () => {
   await Promise.all([setupOffscreenDocument(), setupAction()]);
+});
+
+chrome.runtime.onSuspend.addListener(async () => {
+  await chrome.offscreen.closeDocument();
 });
 
 chrome.runtime.onInstalled.addListener(async () => {
