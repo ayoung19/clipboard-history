84fc548684bbb3a0e42e13b84f9a0f9e26f9ec6f
ayoung19 <andyluyoung@gmail.com>
2024-09-20 07:21:15 -0700

    chore: remove unused code

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,16 +1,12 @@
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useAtomValue } from "jotai";
 
 import { App } from "./App";
-import { settingsAtom } from "./states/atoms";
 
 const queryClient = new QueryClient();
 
 export default function IndexPopup() {
-  const settings = useAtomValue(settingsAtom);
-
   return (
     <MantineProvider theme={{ cursorType: "pointer" }} withGlobalStyles withNormalizeCSS>
       <ModalsProvider>
