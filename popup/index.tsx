dc29704bfc3ef3df8de5cbc030d09639515e4587
ayoung19 <andyluyoung@gmail.com>
2024-09-20 18:56:21 -0700

    fix: change dark.0 to gray.3

--- popup/index.tsx
+++ popup/index.tsx
@@ -26,9 +26,24 @@ export default function IndexPopup() {
       theme={{
         cursorType: "pointer",
         colorScheme: themeToColorScheme(settings.theme) || systemColorScheme,
-        black: "#343a40",
+        black: "#343a40", // gray.8
         primaryColor: "indigo",
         primaryShade: { light: 3, dark: 7 },
+        colors: {
+          // Change dark.0 to gray.3, keep the rest.
+          dark: [
+            "#dee2e6",
+            "#A6A7AB",
+            "#909296",
+            "#5c5f66",
+            "#373A40",
+            "#2C2E33",
+            "#25262b",
+            "#1A1B1E",
+            "#141517",
+            "#101113",
+          ],
+        },
       }}
       withGlobalStyles
       withNormalizeCSS
