e0d2559aef7d5b8f5dba3ea9667a50f3ea525057
Rhythm Aich <rhythmaich@gmail.com>
2024-09-30 01:07:29 +0530

    Added checks to exclude image links in entries

--- types/entry.ts
+++ types/entry.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
+const imageUrlPattern = /\.(jpeg|jpg|png|gif|bmp|svg|webp)$/i;
+
 export const Entry = z.object({
   id: z.string(),
   createdAt: z.number(),
-  content: z.string(),
+  content: z.string().refine((value) => !imageUrlPattern.test(value)),
 });
 export type Entry = z.infer<typeof Entry>;
