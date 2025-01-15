42f95376b44e1be091fe348b2bba95f4f5f99f66
Rhythm Aich <rhythmaich@gmail.com>
2025-01-15 22:02:57 +0530

    Added recommended sizes

--- types/popupWindowSize.ts
+++ types/popupWindowSize.ts
@@ -4,9 +4,9 @@ export const PopupSize = z.enum(["sm", "md", "lg"]);
 export type PopupSize = z.infer<typeof PopupSize>;
 
 export const PopupSizeMap = {
-  sm: { height: 300, width: 500 },
-  md: { height: 500, width: 700 },
-  lg: { height: 600, width: 800 },
+  sm: { height: 400, width: 300 },
+  md: { height: 500, width: 500 },
+  lg: { height: 600, width: 700 },
 } as const;
 
 export type PopupSizeDimensions = (typeof PopupSizeMap)[PopupSize];
