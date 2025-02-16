19d81565ec9067e37fd15ebe0ec5a5c4acaaa342
ayoung19 <andyluyoung@gmail.com>
2025-02-15 21:18:16 -0800

    Remove link in modal

--- popup/components/modals/ShortcutsModalContent.tsx
+++ popup/components/modals/ShortcutsModalContent.tsx
@@ -1,16 +1,17 @@
 import {
-  Anchor,
   CloseButton,
   Group,
   Kbd,
   Paper,
   SegmentedControl,
-  Stack,
   Text,
   Title,
+  Tooltip,
+  UnstyledButton,
 } from "@mantine/core";
+import { useClipboard } from "@mantine/hooks";
 import { modals } from "@mantine/modals";
-import { IconKeyboardOff } from "@tabler/icons-react";
+import { IconCopy, IconKeyboardOff } from "@tabler/icons-react";
 import { useAtomValue } from "jotai";
 
 import { commandsAtom, entryCommandsAtom } from "~popup/states/atoms";
@@ -22,6 +23,7 @@ interface Props {
 }
 
 export const ShortcutsModalContent = ({ entry }: Props) => {
+  const clipboard = useClipboard();
   const entryCommands = useAtomValue(entryCommandsAtom);
   const commands = useAtomValue(commandsAtom);
 
@@ -29,81 +31,71 @@ export const ShortcutsModalContent = ({ entry }: Props) => {
 
   return (
     <Paper p="md">
-      <Stack spacing="xs">
-        <Stack spacing={0}>
-          <Group align="center" position="apart" mb="xs">
-            <Title order={5}>Assign a Shortcut to this Entry</Title>
-            <CloseButton onClick={() => modals.closeAll()} />
-          </Group>
-          <Text fz="xs">This will overwrite any existing entry assigned to the shortcut.</Text>
-        </Stack>
-
-        <Stack align="center" spacing="xs">
-          <SegmentedControl
-            size="md"
-            value={
-              entryCommands.find((entryCommand) => entryCommand.entryId === entry.id)
-                ?.commandName || ""
-            }
-            onChange={(value) =>
-              value.length === 0
-                ? deleteEntryCommand(entry.id)
-                : createEntryCommand(entry.id, value)
-            }
-            data={[
-              ...commands
-                .filter((command) => command.name.startsWith("paste-item"))
-                .slice()
-                .sort((a, b) => a.name.localeCompare(b.name))
-                .map((command) => ({
-                  label: command.shortcut ? (
-                    <Group align="center" spacing={4} noWrap>
-                      {command.shortcut.split("").map((c, i) => (
-                        <>
-                          {i > 0 && <>+</>}
-                          <Kbd>{c}</Kbd>
-                        </>
-                      ))}
-                    </Group>
-                  ) : (
-                    <Group align="center" noWrap>
-                      <Kbd>Not set</Kbd>
-                    </Group>
-                  ),
-                  value: command.name,
-                })),
-              {
-                label: (
-                  <Group align="center" noWrap h="100%">
-                    <IconKeyboardOff />
+      <Group align="center" position="apart" mb="xs">
+        <Title order={5}>Assign Shortcut to Item</Title>
+        <CloseButton onClick={() => modals.closeAll()} />
+      </Group>
+      <Text fz="xs" mb="xs">
+        Assign a keyboard shortcut to this item. To customize shortcuts, visit
+        <> </>
+        <Tooltip label={<Text fz="xs">{clipboard.copied ? "Copied" : "Copy"}</Text>}>
+          <UnstyledButton onClick={() => clipboard.copy("chrome://extensions/shortcuts")}>
+            <Group align="center" spacing={4}>
+              <Text fz="xs">chrome://extensions/shortcuts</Text>
+              <IconCopy size="0.8rem" />
+            </Group>
+          </UnstyledButton>
+        </Tooltip>
+        .
+      </Text>
+      <Group align="center" position="center">
+        <SegmentedControl
+          size="md"
+          value={
+            entryCommands.find((entryCommand) => entryCommand.entryId === entry.id)?.commandName ||
+            ""
+          }
+          onChange={(value) =>
+            value.length === 0 ? deleteEntryCommand(entry.id) : createEntryCommand(entry.id, value)
+          }
+          data={[
+            ...commands
+              .filter((command) => command.name.startsWith("paste-item"))
+              .slice()
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((command) => ({
+                label: command.shortcut ? (
+                  <Group align="center" spacing={4} noWrap>
+                    {command.shortcut.split("").map((c, i) => (
+                      <>
+                        {i > 0 && <>+</>}
+                        <Kbd>{c}</Kbd>
+                      </>
+                    ))}
+                  </Group>
+                ) : (
+                  <Group align="center" noWrap>
+                    <Kbd>Not set</Kbd>
                   </Group>
                 ),
-                value: "",
-              },
-            ]}
-            sx={{
-              ".mantine-SegmentedControl-label": {
-                height: "100%",
-              },
-            }}
-          />
-          <Text fz="xs">
-            You can customize these options by navigating to{" "}
-            <Anchor href="chrome://extensions/shortcuts" target="_blank">
-              chrome://extensions/shortcuts
-            </Anchor>
-          </Text>
-        </Stack>
-
-        {/* <Group align="end" position="right" spacing="xs">
-          <Button size="xs" variant="subtle" onClick={() => setSelectedShortcutKey("")}>
-            Reset
-          </Button>
-          <Button size="xs" onClick={handleSave}>
-            Save
-          </Button>
-        </Group> */}
-      </Stack>
+                value: command.name,
+              })),
+            {
+              label: (
+                <Group align="center" noWrap h="100%">
+                  <IconKeyboardOff />
+                </Group>
+              ),
+              value: "",
+            },
+          ]}
+          sx={{
+            ".mantine-SegmentedControl-label": {
+              height: "100%",
+            },
+          }}
+        />
+      </Group>
     </Paper>
   );
 };
