a291440d6e273aba6fc338a446746acf44ae4eef
ayoung19 <andyluyoung@gmail.com>
2025-03-11 22:33:55 -0700

    fix: offline behavior for firefox

--- background/index.ts
+++ background/index.ts
@@ -33,12 +33,37 @@ if (process.env.PLASMO_TARGET === "firefox-mv2") {
   watchCloudEntries(window, getRefreshToken, async () => {
     await Promise.all([
       handleUpdateContextMenusRequest(),
-      async () => {
+      (async () => {
         const entries = await getEntries();
 
         await handleUpdateTotalItemsBadgeRequest(entries.length);
-      },
+      })(),
     ]);
+  });
+
+  db.subscribeConnectionStatus(async (connectionStatus) => {
+    if (connectionStatus === "opened" || connectionStatus === "closed") {
+      await Promise.all([
+        handleUpdateContextMenusRequest(),
+        (async () => {
+          const entries = await getEntries();
+
+          await handleUpdateTotalItemsBadgeRequest(entries.length);
+        })(),
+      ]);
+
+      // Retry just in case updating the service worker's reactor status takes some time.
+      await new Promise((r) => setTimeout(r, 800));
+
+      await Promise.all([
+        handleUpdateContextMenusRequest(),
+        (async () => {
+          const entries = await getEntries();
+
+          await handleUpdateTotalItemsBadgeRequest(entries.length);
+        })(),
+      ]);
+    }
   });
 }
 
