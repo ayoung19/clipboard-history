fc014a63efe0620d078b0a4b50a8ea986ec94eb9
rishilahoti <rishilahoti99@gmail.com>
2025-03-27 15:24:56 +0530

    format changed

--- popup/pages/AllPage.tsx
+++ popup/pages/AllPage.tsx
@@ -7,28 +7,28 @@ import { useEntryIdToTags } from "~popup/contexts/EntryIdToTagsContext";
 import { searchAtom } from "~popup/states/atoms";
 
 export const AllPage = () => {
-    const reversedEntries = useEntries();
-    const search = useAtomValue(searchAtom);
-    const entryIdToTags = useEntryIdToTags();
+  const reversedEntries = useEntries();
+  const search = useAtomValue(searchAtom);
+  const entryIdToTags = useEntryIdToTags();
 
-    return (
-        <EntryList
-            noEntriesOverlay={
-                search.length === 0 ? (
-                    <NoEntriesOverlay
-                        title="Your clipboard history is empty"
-                        subtitle="Copy any text to see it here"
-                    />
-                ) : (
-                    <NoEntriesOverlay title={`No items found for "${search}"`} />
-                )
-            }
-            entries={reversedEntries.filter(
-                (entry) =>
-                    search.length === 0 ||
-                    entry.content.toLowerCase().includes(search.toLowerCase()) ||
-                    entryIdToTags[entry.id]?.some((tag) => tag.includes(search.toLowerCase())),
-            )}
-        />
-    );
+  return (
+    <EntryList
+      noEntriesOverlay={
+        search.length === 0 ? (
+          <NoEntriesOverlay
+            title="Your clipboard history is empty"
+            subtitle="Copy any text to see it here"
+          />
+        ) : (
+          <NoEntriesOverlay title={`No items found for "${search}"`} />
+        )
+      }
+      entries={reversedEntries.filter(
+        (entry) =>
+          search.length === 0 ||
+          entry.content.toLowerCase().includes(search.toLowerCase()) ||
+          entryIdToTags[entry.id]?.some((tag) => tag.includes(search.toLowerCase())),
+      )}
+    />
+  );
 };
