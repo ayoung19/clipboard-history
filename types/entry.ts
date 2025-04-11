f8710c49f940ce3043cd6e3cb47c6c5a35d8fcb4
ayoung19 <andyluyoung@gmail.com>
2025-04-11 12:40:28 -0700

    fix: import bug

--- types/entry.ts
+++ types/entry.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const Entry = z.object({
   id: z.string(),
   createdAt: z.number(),
-  copiedAt: z.number().optional(),
+  copiedAt: z.number().nullable().optional(),
   content: z.string(),
 });
 export type Entry = z.infer<typeof Entry>;
